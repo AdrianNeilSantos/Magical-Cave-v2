dcc8f1424da3cbd91dc85fd618d6f10eeeac169b
Adrian Santos <apsantos8@up.edu.ph>
2022-01-14 17:18:56 +0800

    added currency

---
+++ Assets/CurrencyUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CurrencyUI : MonoBehaviour
+{
+    TextMeshProUGUI score;
+
+
+    void Start(){
+        score = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update() {
+        score.text = "Currency: " + Currency.getCurrency();
+    }
+
+
+
+}
